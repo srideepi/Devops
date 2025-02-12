1768b0dce5f460fa888a40237b9a6020c2c2f138
athika.26csa@licet.ac.in <jovinj1412@gmail.com>
2025-02-12 12:10:02 +0530

    Initial commit

---
+++ src/main/java/com/athika/lattefy/lattefy/LattefyApplication.java
@@ -0,0 +1,13 @@
+package com.athika.lattefy.lattefy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LattefyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LattefyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/athika/lattefy/lattefy/LattefyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athika.lattefy.lattefy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LattefyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
